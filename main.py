026a4834034e9bc4a4beb22b6ce59b1f5d28cd97
beccarorron <justinhil@gmail.com>
2023-11-24 21:41:07 -0800

    Saving it for the night

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, redirect
+from flask_socketio import join_room, leave_room, send, SocketIO
+import random 
+from string import ascii_uppercase
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
+socketio = SocketIO(app)
+
+@app.route("/", methods = ["POST", "GET"])
+def home():
+    if request.method == ['POST']:
+        name = request.form.get('name')
+        code = request.form.get('code')
+        join = request.form.get('join', False)
+        create = request.form.get('create', False)
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
